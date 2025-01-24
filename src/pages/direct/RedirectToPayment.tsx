7584898bd689888b8e983da6826ae56f749f2c08
Noah <phvohieunghia@gmail.com>
2025-01-24 18:16:07 +0700

    fix: test payment v3

--- src/pages/direct/RedirectToPayment.tsx
+++ src/pages/direct/RedirectToPayment.tsx
@@ -11,6 +11,7 @@ export const RedirectToPayment = () => {
     resultCode: +params.resultCode,
     transId: +params.transId,
   } as MomoRedirectUrlVariables
+  console.log(params)
 
   PaymentService.instance.setInformation(convertedParams)
 
