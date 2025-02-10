cbd70d172657168173db26cb51fd2eb464b9a120
Noah <phvohieunghia@gmail.com>
2025-02-10 14:15:22 +0700

    fix: login with google v3

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -2,12 +2,14 @@ interface AppConfig {
   title: string
   environment: string
   restFullApiUrl: string
+  baseUrl: string
   googleClientId: string
 }
 
 export const appConfig: AppConfig = {
   title: 'MẪU VĂN BẢN.VN' + ((import.meta.env.VITE_ENVIRONMENT ?? '') === 'dev' ? ' - DEV' : ''),
   environment: import.meta.env.VITE_ENVIRONMENT || '',
+  baseUrl: import.meta.env.VITE_BASE_URL || '',
   restFullApiUrl: import.meta.env.VITE_API_BASE_URL || '',
   googleClientId: import.meta.env.VITE_GOOGLE_CLIENT_ID || '',
 }
