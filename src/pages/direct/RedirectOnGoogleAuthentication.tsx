1e8fdc83f74030d9d583b40febe8bdea3e27b7bd
Noah <phvohieunghia@gmail.com>
2025-02-10 11:57:37 +0700

    fix: login with google v2

--- src/pages/direct/RedirectOnGoogleAuthentication.tsx
+++ src/pages/direct/RedirectOnGoogleAuthentication.tsx
@@ -1,13 +1,16 @@
 import { LocalStorageService, URLSearchParamsService } from '@/services'
 import { LOCAL_STORAGE, ROUTE_NAME } from '@/utils'
+import { jwtDecode } from 'jwt-decode'
 import { Navigate } from 'react-router-dom'
 
 export const RedirectOnGoogleAuthentication = () => {
   const x = URLSearchParamsService.get
-  console.log(x)
   Object.entries(x).map(([key, value]) => {
     if (key === 'accessToken') LocalStorageService.instance.set(LOCAL_STORAGE.ACCESS_TOKEN, value)
     else if (key === 'refreshToken') LocalStorageService.instance.set(LOCAL_STORAGE.REFRESH_TOKEN, value)
+    else if (key === 'code') {
+      console.log(jwtDecode(value))
+    }
   })
   return null
   return <Navigate to={ROUTE_NAME.HOME} />
